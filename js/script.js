1ec78aa8ea52e4f2eb4b60bf536c86286a4d5a9f
az1dii <azheng1092@gmail.com>
2021-09-01 23:47:21 -0400

    update

--- js/script.js
+++ js/script.js
@@ -6,8 +6,23 @@ const $input = $('input[type="text"]');
 const $pName = $('#name');
 const $pImg = $('#poster');
 const $pId =$('#id');
+
+// STATS
 const $pStats =$('#stats');
-const $pStatsn = $('#statsn');
+const $pStats1 =$('#stats1');
+const $pStats2 =$('#stats2');
+const $pStats3 =$('#stats3');
+const $pStats4 =$('#stats4');
+const $pStats5 =$('#stats5');
+
+const $pStatsn =$('#statsn');
+const $pStatsn1 =$('#statsn1');
+const $pStatsn2 =$('#statsn2');
+const $pStatsn3 =$('#statsn3');
+const $pStatsn4 =$('#statsn4');
+const $pStatsn5 =$('#statsn5');
+
+
 const $pTypes =$('#types');
 
 
@@ -56,18 +71,34 @@ function render(){
    $pName.html(pokeDB.name);
    $pTypes.html(pokeDB.types.map((type) => type.type.name).join(', '));
    $pId.html(pokeDB.id);
-   $pStats.html(pokeDB.stats.map((stat) =>stat.stat.name).join(', '));
-   $pStatsn.html(pokeDB.stats.map((stat) =>stat.base_stat).join(', '));
-   
+
+   $pStats.html(pokeDB.stats[0].stat.name)
+   $pStats1.html(pokeDB.stats[1].stat.name)
+   $pStats2.html(pokeDB.stats[2].stat.name)
+   $pStats3.html(pokeDB.stats[3].stat.name)
+   $pStats4.html(pokeDB.stats[4].stat.name)
+   $pStats5.html(pokeDB.stats[5].stat.name)
   
+  //  $pStatsn.html(pokeDB.stats.map((stat) =>stat.stat.base_stat).join(' | '));
+
+   $pStatsn.html(pokeDB.stats[0].stat.base_stat)
+   $pStatsn1.html(pokeDB.stats[1].stat.base_stat)
+   $pStatsn2.html(pokeDB.stats[2].stat.base_stat)
+   $pStatsn3.html(pokeDB.stats[3].stat.base_stat)
+   $pStatsn4.html(pokeDB.stats[4].stat.base_stat)
+   $pStatsn5.html(pokeDB.stats[5].stat.base_stat)
+
    $weight.html(pokeDB.weight);
    $height.html(pokeDB.height);
   
    $pImg.attr("src", pokeDB.sprites['front_default']);
   
+  
+
+
+
+
 }
-
-
 
 
 $('form').on('submit', fetch);
