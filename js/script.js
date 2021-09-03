86f6030ec20d823734a825839a894495399e12d4
az1dii <azheng1092@gmail.com>
2021-09-02 23:06:57 -0400

    update

--- js/script.js
+++ js/script.js
@@ -26,31 +26,12 @@ const $pStatsn5 =$('#statsn5');
 const $pTypes =$('#types');
 
 
-const $weight = $('#weight');
-const $height = $('#height');
 
-
-// types colors
-const typeColors = {
-	fire: "#FDDFDF",
-	grass: "#DEFDE0",
-	electric: "#FCF7DE",
-	water: "#DEF3FD",
-	ground: "#f4e7da",
-	rock: "#d5d5d4",
-	fairy: "#fceaff",
-	poison: "#98d7a5",
-	bug: "#f8d5a3",
-	dragon: "#97b3e6",
-	psychic: "#eaeda1",
-	flying: "#F5F5F5",
-	fighting: "#E6E0D4",
-	normal: "#F5F5F5",
-};
 
 function fetch(event){
   event.preventDefault();
 
+  //API setup
   $.ajax({
     url: `https://pokeapi.co/api/v2/pokemon/${$input.val()}`
   }).then(
@@ -66,38 +47,30 @@ function fetch(event){
   )
 }
 
-
+//fetch data
 function render(){
    $pName.html(pokeDB.name);
    $pTypes.html(pokeDB.types.map((type) => type.type.name).join(', '));
-   $pId.html(pokeDB.id);
-
+   $pId.html("#"+ pokeDB.id);
+  
+  //Stats Names
    $pStats.html(pokeDB.stats[0].stat.name)
    $pStats1.html(pokeDB.stats[1].stat.name)
    $pStats2.html(pokeDB.stats[2].stat.name)
    $pStats3.html(pokeDB.stats[3].stat.name)
    $pStats4.html(pokeDB.stats[4].stat.name)
    $pStats5.html(pokeDB.stats[5].stat.name)
-  
-  //  $pStatsn.html(pokeDB.stats.map((stat) =>stat.stat.base_stat).join(' | '));
 
-   $pStatsn.html(pokeDB.stats[0].stat.base_stat)
-   $pStatsn1.html(pokeDB.stats[1].stat.base_stat)
-   $pStatsn2.html(pokeDB.stats[2].stat.base_stat)
-   $pStatsn3.html(pokeDB.stats[3].stat.base_stat)
-   $pStatsn4.html(pokeDB.stats[4].stat.base_stat)
-   $pStatsn5.html(pokeDB.stats[5].stat.base_stat)
-
-   $weight.html(pokeDB.weight);
-   $height.html(pokeDB.height);
+  //Stats Numbers
+   $pStatsn.html(pokeDB.stats[0].base_stat)
+   $pStatsn1.html(pokeDB.stats[1].base_stat)
+   $pStatsn2.html(pokeDB.stats[2].base_stat)
+   $pStatsn3.html(pokeDB.stats[3].base_stat)
+   $pStatsn4.html(pokeDB.stats[4].base_stat)
+   $pStatsn5.html(pokeDB.stats[5].base_stat)
   
+  //Pokemon Img
    $pImg.attr("src", pokeDB.sprites['front_default']);
-  
-  
-
-
-
-
 }
 
 
